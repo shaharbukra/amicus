e2aaff2165d32e1a7d52279ece1cb916902023b3
Shahar Bukra <shaharbukra@gmail.com>
2019-03-07 16:02:31 +0200

    added checkbox and built the app

---
+++ htmlTemplate/build/precache-manifest.6ceebda566df17d7151a4f43a9898bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c8223bcbdc8793fb70a9",
+    "url": "/static/js/main.c8223bcb.chunk.js"
+  },
+  {
+    "revision": "8f21339fb5726aa8b36b",
+    "url": "/static/js/2.8f21339f.chunk.js"
+  },
+  {
+    "revision": "c8223bcbdc8793fb70a9",
+    "url": "/static/css/main.3a318ab0.chunk.css"
+  },
+  {
+    "revision": "53ac2ca9485aeb420538889767f1c121",
+    "url": "/index.html"
+  }
+];
--- src/components/ClickedTable/index.jsx
+++ src/components/ClickedTable/index.jsx
@@ -1,33 +1,74 @@
 import React, { Component } from "react";
+import Checkbox from "../Checkbox/Checkbox";
 
 import "./clickedTable.css";
 
 export class ClickedTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkedItems: -1
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+      this.setState({ checkedItems: item });
+    } else {
+      this.setState({ checkedItems: -1 });
+    }
+  }
 
   render() {
-    const peopleRows = Object.keys( this.props.people )
-            .map( igKey => {
-                const {name, gender, height,mass,hair_color,skin_color,eye_color,birth_year} = this.props.people[igKey]
-                return (
-                  <React.Fragment key={igKey}>
-                  <tr className="row" >
-                    <td data-column="Name">{name}</td>
-                    <td data-column="Gender">{gender}</td>
-                    <td data-column="Height">{height}</td>
-                    <td data-column="Birth Year">{birth_year}</td>
-                    <td data-column="Mass">{mass}</td>
-                    <td data-column="Hair Color">{hair_color}</td>
-                    <td data-column="Skin Color">{skin_color}</td>
-                  </tr>
-                  <tr className="desc" >
-                    <td colSpan="7">
-                     Eye Color : {eye_color}
-                    </td>
-                  </tr>
-                </React.Fragment>
-               );
-            } );
-            console.log(peopleRows);
+    let selectedRow = this.state.checkedItems;
+    const peopleRows = Object.keys(this.props.people).map(igKey => {
+      const {
+        name,
+        gender,
+        height,
+        mass,
+        hair_color,
+        skin_color,
+        eye_color,
+        birth_year
+      } = this.props.people[igKey];
+      let rowClassName = "row";
+
+      if (selectedRow && selectedRow === igKey) {
+        rowClassName += " selectedRow";
+      }
+
+      return (
+        <React.Fragment key={igKey}>
+          <tr className={rowClassName}>
+            <td data-column="Name">
+              <Checkbox
+                name={igKey}
+                checked={selectedRow === igKey}
+                onChange={this.handleChange}
+              />
+              {name}
+            </td>
+            <td data-column="Gender">{gender}</td>
+            <td data-column="Height">{height}</td>
+            <td data-column="Birth Year">{birth_year}</td>
+            <td data-column="Mass">{mass}</td>
+            <td data-column="Hair Color">{hair_color}</td>
+            <td data-column="Skin Color">{skin_color}</td>
+          </tr>
+          <tr className="desc">
+            <td data-column="Additional Data" colSpan="7">Eye Color : {eye_color}</td>
+          </tr>
+        </React.Fragment>
+      );
+    });
+    console.log(peopleRows);
     return (
       <div>
         <header>
@@ -46,15 +87,12 @@ export class ClickedTable extends Component {
                 <th>Skin Color</th>
               </tr>
             </thead>
-            <tbody>
-             {peopleRows}
-            </tbody>
+            <tbody>{peopleRows}</tbody>
           </table>
         </main>
       </div>
     );
   }
 }
-
 
 export default ClickedTable;
